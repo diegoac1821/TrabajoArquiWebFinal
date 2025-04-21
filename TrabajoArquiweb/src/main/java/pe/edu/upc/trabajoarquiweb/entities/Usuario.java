79f69318903547393182ca832493330ab4723946
JoseLuis <u202219637@upc.edu.pe>
2025-04-21 17:09:18 -0500

    asdadsasdasdasdasd

--- TrabajoArquiweb/src/main/java/pe/edu/upc/trabajoarquiweb/entities/Usuario.java
+++ TrabajoArquiweb/src/main/java/pe/edu/upc/trabajoarquiweb/entities/Usuario.java
@@ -1,4 +1,10 @@
 package pe.edu.upc.trabajoarquiweb.entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 public class Usuario {
+    @Id
+    @GeneratedValue
+    private int id;
 }
