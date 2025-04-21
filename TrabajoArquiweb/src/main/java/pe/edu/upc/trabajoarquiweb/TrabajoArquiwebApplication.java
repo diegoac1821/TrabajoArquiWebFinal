57fed67f59fe6063089b6c56248b0e573ccbe270
diegoalhuay <diegoac1820@gmail.com>
2025-04-20 21:45:43 -0500

    primer commit

---
+++ TrabajoArquiweb/src/main/java/pe/edu/upc/trabajoarquiweb/TrabajoArquiwebApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.trabajoarquiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoArquiwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabajoArquiwebApplication.class, args);
+    }
+
+}
