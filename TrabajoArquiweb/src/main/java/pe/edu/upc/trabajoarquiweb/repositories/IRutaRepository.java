c3fb65f25ec301325c2c88b3a0850c0fbfcf6727
diegoalhuay <diegoac1820@gmail.com>
2025-04-22 14:11:51 -0500

    se agrego @repository a los q faltaban

--- TrabajoArquiweb/src/main/java/pe/edu/upc/trabajoarquiweb/repositories/IRutaRepository.java
+++ TrabajoArquiweb/src/main/java/pe/edu/upc/trabajoarquiweb/repositories/IRutaRepository.java
@@ -1,7 +1,8 @@
 package pe.edu.upc.trabajoarquiweb.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pe.edu.upc.trabajoarquiweb.entities.Ruta;
-
+@Repository
 public interface IRutaRepository extends JpaRepository<Ruta, Integer> {
 }
