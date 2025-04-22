c12a2379d1474680c0c1d92dd2a6d97c8b2700bc
JoseLuis <u202219637@upc.edu.pe>
2025-04-22 16:34:50 -0500

    comit prueba tabla usuario no completa

--- TrabajoArquiweb/src/main/java/pe/edu/upc/trabajoarquiweb/entities/Usuario.java
+++ TrabajoArquiweb/src/main/java/pe/edu/upc/trabajoarquiweb/entities/Usuario.java
@@ -1,10 +1,5 @@
 package pe.edu.upc.trabajoarquiweb.entities;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-
 public class Usuario {
-    @Id
-    @GeneratedValue
-    private int id;
+
 }
