c1ffdd9d38428660b7d98cdffce10c8338b4c12d
Heiner-Pinares <u202212258@upc.edu.pe>
2025-04-27 18:07:35 -0500

    Corrigiendo cositas 2.0 >;)

--- TrabajoArquiweb/src/main/java/pe/edu/upc/trabajoarquiweb/securities/WebSecurityConfig.java
+++ TrabajoArquiweb/src/main/java/pe/edu/upc/trabajoarquiweb/securities/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package pe.edu.upc.trabajoarquiweb.securities;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +18,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
